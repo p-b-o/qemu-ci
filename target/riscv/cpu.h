eac312f38c2beffeb6e2b6d19b70e5c28891f91f
Daniel Henrique Barboza <daniel.barboza@oss.qualcomm.com>
2026-07-02 10:51:10 -0300

    target/riscv: remove csr.h from kvm-cpu.c
    Move riscv_new_csr_seed from csr.c to cpu.c since this function is
    shared with KVM.  With that we can remove the csr.h from kvm-cpu.c.
    
    Signed-off-by: Daniel Henrique Barboza <daniel.barboza@oss.qualcomm.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Link: https://lore.kernel.org/qemu-devel/20260702135128.1188021-9-daniel.barboza@oss.qualcomm.com

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -972,7 +972,8 @@ bool riscv_cpu_accelerator_compatible(RISCVCPU *cpu);
 extern const bool valid_vm_1_10_32[], valid_vm_1_10_64[];
 
 void riscv_cpu_register_gdb_regs_for_features(CPUState *cs);
-
+target_ulong riscv_new_csr_seed(target_ulong new_value,
+                                target_ulong write_mask);
 const char *satp_mode_str(uint8_t satp_mode, bool is_32_bit);
 
 const char *priv_spec_to_str(int priv_version);
