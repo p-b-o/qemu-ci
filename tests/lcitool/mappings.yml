qemu/qemu

commit 2a428c8541ec5e9be54aad5f0a446ec8172226c9

dockerfiles: remove packages required by Avocado
Make our CI containers smaller, by removing a handful of packages that
were only included as dependencies of Avocado.

Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

.gitlab-ci.d/macos-14.vars
@@ -12,5 +12,5 @@ NINJA='/opt/homebrew/bin/ninja'
 PACKAGING_COMMAND='brew'
 PIP3='/opt/homebrew/bin/pip3'
 PKGS='bash bc bindgen bison bzip2 capstone ccache cmocka coreutils ctags curl dbus diffutils dtc flex gcovr gettext git glib gnu-sed gnutls gtk+3 gtk-vnc jemalloc jpeg-turbo json-c libcbor libepoxy libffi libgcrypt libiscsi libnfs libpng libslirp libssh libtasn1 libusb llvm lzo make meson mtools ncurses nettle ninja pixman pkg-config python-setuptools python3 rpm2cpio rust sdl2 sdl2_image snappy socat sparse spice-protocol swtpm tesseract usbredir vde vte3 vulkan-tools xorriso zlib zstd'
-PYPI_PKGS='PyYAML numpy pillow sphinx sphinx-rtd-theme tomli'
+PYPI_PKGS='sphinx sphinx-rtd-theme tomli'
 PYTHON='/opt/homebrew/bin/python3'

scripts/ci/setup/debian/debian-13-ppc64le.yaml
@@ -105,9 +105,6 @@ packages:
   - openssh-client
   - pkgconf
   - python3
-  - python3-numpy
-  - python3-opencv
-  - python3-pillow
   - python3-pip
   - python3-setuptools
   - python3-sphinx
@@ -115,7 +112,6 @@ packages:
   - python3-tomli
   - python3-venv
   - python3-wheel
-  - python3-yaml
   - rpm2cpio
   - rustc
   - sed

scripts/ci/setup/ubuntu/ubuntu-2404-aarch64.yaml
@@ -106,9 +106,6 @@ packages:
   - openssh-client
   - pkgconf
   - python3
-  - python3-numpy
-  - python3-opencv
-  - python3-pillow
   - python3-pip
   - python3-setuptools
   - python3-sphinx
@@ -116,7 +113,6 @@ packages:
   - python3-tomli
   - python3-venv
   - python3-wheel
-  - python3-yaml
   - rpm2cpio
   - rustc-1.83
   - sed

scripts/ci/setup/ubuntu/ubuntu-2404-s390x.yaml
@@ -104,9 +104,6 @@ packages:
   - openssh-client
   - pkgconf
   - python3
-  - python3-numpy
-  - python3-opencv
-  - python3-pillow
   - python3-pip
   - python3-setuptools
   - python3-sphinx
@@ -114,7 +111,6 @@ packages:
   - python3-tomli
   - python3-venv
   - python3-wheel
-  - python3-yaml
   - rpm2cpio
   - rustc-1.83
   - sed

tests/docker/dockerfiles/alpine.docker
@@ -83,14 +83,11 @@ RUN apk update && \
         pixman-dev \
         pkgconf \
         pulseaudio-dev \
-        py3-numpy \
-        py3-pillow \
         py3-pip \
         py3-setuptools \
         py3-sphinx \
         py3-sphinx_rtd_theme \
         py3-wheel \
-        py3-yaml \
         python3 \
         rpm2cpio \
         rust \

tests/docker/dockerfiles/centos9.docker
@@ -96,9 +96,6 @@ RUN dnf --quiet distro-sync -y && \
                 pkgconfig \
                 pulseaudio-libs-devel \
                 python3 \
-                python3-PyYAML \
-                python3-numpy \
-                python3-pillow \
                 python3-pip \
                 python3-setuptools \
                 python3-sphinx \

tests/docker/dockerfiles/debian-amd64-cross.docker
@@ -41,9 +41,6 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
-                      python3-numpy \
-                      python3-opencv \
-                      python3-pillow \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \
@@ -51,7 +48,6 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       python3-tomli \
                       python3-venv \
                       python3-wheel \
-                      python3-yaml \
                       rpm2cpio \
                       rustc \
                       sed \

tests/docker/dockerfiles/debian-arm64-cross.docker
@@ -41,9 +41,6 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
-                      python3-numpy \
-                      python3-opencv \
-                      python3-pillow \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \
@@ -51,7 +48,6 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       python3-tomli \
                       python3-venv \
                       python3-wheel \
-                      python3-yaml \
                       rpm2cpio \
                       rustc \
                       sed \

p-b-o/qemu-ci

commit f8706e21d643dd346bb3294d5ee1ade9abe1c685

tests: re-add opencv2 and numpy dependencies
Fixes: 2a428c8541ec5e9be54aad5f0a446ec8172226c9

Some of the dependencies identified as avocado dependencies were in
fact still being used, and should be re-added to our lcitool
configuration to ensure that these tests are being executed in the CI
environment.

Signed-off-by: John Snow <jsnow@redhat.com>
Link: https://lore.kernel.org/qemu-devel/20260824052859.880475-2-jsnow@redhat.com

.gitlab-ci.d/macos-14.vars
@@ -12,5 +12,5 @@ NINJA='/opt/homebrew/bin/ninja'
 PACKAGING_COMMAND='brew'
 PIP3='/opt/homebrew/bin/pip3'
 PKGS='bash bc bindgen bison bzip2 capstone ccache cmocka coreutils ctags curl dbus diffutils dtc flex gcovr gettext git glib gnu-sed gnutls gtk+3 gtk-vnc jemalloc jpeg-turbo json-c libcbor libepoxy libffi libgcrypt libiscsi libnfs libpng libslirp libssh libtasn1 libusb llvm lzo make meson mtools ncurses nettle ninja pixman pkg-config python-setuptools python3 rpm2cpio rust sdl2 sdl2_image snappy socat sparse spice-protocol swtpm tesseract usbredir vde vte3 vulkan-tools xorriso zlib zstd'
-PYPI_PKGS='sphinx sphinx-rtd-theme tomli'
+PYPI_PKGS='numpy sphinx sphinx-rtd-theme tomli'
 PYTHON='/opt/homebrew/bin/python3'

scripts/ci/setup/debian/debian-13-ppc64le.yaml
@@ -105,6 +105,8 @@ packages:
   - openssh-client
   - pkgconf
   - python3
+  - python3-numpy
+  - python3-opencv
   - python3-pip
   - python3-setuptools
   - python3-sphinx

scripts/ci/setup/ubuntu/ubuntu-2404-aarch64.yaml
@@ -106,6 +106,8 @@ packages:
   - openssh-client
   - pkgconf
   - python3
+  - python3-numpy
+  - python3-opencv
   - python3-pip
   - python3-setuptools
   - python3-sphinx

scripts/ci/setup/ubuntu/ubuntu-2404-s390x.yaml
@@ -104,6 +104,8 @@ packages:
   - openssh-client
   - pkgconf
   - python3
+  - python3-numpy
+  - python3-opencv
   - python3-pip
   - python3-setuptools
   - python3-sphinx

tests/docker/dockerfiles/alpine.docker
@@ -83,6 +83,7 @@ RUN apk update && \
         pixman-dev \
         pkgconf \
         pulseaudio-dev \
+        py3-numpy \
         py3-pip \
         py3-setuptools \
         py3-sphinx \

tests/docker/dockerfiles/centos9.docker
@@ -130,6 +130,8 @@ RUN dnf --quiet distro-sync -y && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
 RUN /usr/bin/pip3.11 install \
+                     numpy \
+                     opencv-python \
                      sphinx \
                      sphinx-rtd-theme
 

tests/docker/dockerfiles/debian-amd64-cross.docker
@@ -41,6 +41,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/docker/dockerfiles/debian-arm64-cross.docker
@@ -41,6 +41,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/docker/dockerfiles/debian-armhf-cross.docker
@@ -41,6 +41,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/docker/dockerfiles/debian-i686-cross.docker
@@ -41,6 +41,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/docker/dockerfiles/debian-ppc64el-cross.docker
@@ -41,6 +41,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/docker/dockerfiles/debian-riscv64-cross.docker
@@ -41,6 +41,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/docker/dockerfiles/debian-s390x-cross.docker
@@ -41,6 +41,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/docker/dockerfiles/debian.docker
@@ -113,6 +113,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/docker/dockerfiles/fedora-rust-nightly.docker
@@ -104,6 +104,8 @@ exec "$@"\n' > /usr/bin/nosync && \
                        pkgconfig \
                        pulseaudio-libs-devel \
                        python3 \
+                       python3-numpy \
+                       python3-opencv \
                        python3-pip \
                        python3-setuptools \
                        python3-sphinx \

tests/docker/dockerfiles/fedora-win64-cross.docker
@@ -44,6 +44,8 @@ exec "$@"\n' > /usr/bin/nosync && \
                        ninja-build \
                        openssh-clients \
                        python3 \
+                       python3-numpy \
+                       python3-opencv \
                        python3-pip \
                        python3-setuptools \
                        python3-sphinx \

tests/docker/dockerfiles/fedora.docker
@@ -104,6 +104,8 @@ exec "$@"\n' > /usr/bin/nosync && \
                        pkgconfig \
                        pulseaudio-libs-devel \
                        python3 \
+                       python3-numpy \
+                       python3-opencv \
                        python3-pip \
                        python3-setuptools \
                        python3-sphinx \

tests/docker/dockerfiles/opensuse-leap.docker
@@ -95,6 +95,8 @@ RUN zypper update -y && \
            pkgconfig \
            python3-Sphinx \
            python3-base \
+           python3-numpy \
+           python3-opencv \
            python3-pip \
            python3-setuptools \
            python3-sphinx_rtd_theme \

tests/docker/dockerfiles/ubuntu2404.docker
@@ -113,6 +113,8 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
                       openssh-client \
                       pkgconf \
                       python3 \
+                      python3-numpy \
+                      python3-opencv \
                       python3-pip \
                       python3-setuptools \
                       python3-sphinx \

tests/lcitool/mappings.yml
@@ -21,6 +21,12 @@ mappings:
   python3-sphinx-rtd-theme:
     CentOSStream9:
 
+  python3-numpy:
+    CentOSStream9:
+
+  python3-opencv:
+    CentOSStream9:
+
   python3-venv:
     CentOSStream9: python3.11
 
@@ -54,3 +60,9 @@ pypi_mappings:
     apk:
     Fedora:
     Debian12:
+
+  python3-numpy:
+    CentOSStream9: numpy
+
+  python3-opencv:
+    CentOSStream9: opencv-python

tests/lcitool/projects/qemu.yml
@@ -91,6 +91,8 @@ packages:
  - pkg-config
  - pulseaudio
  - python3
+ - python3-numpy
+ - python3-opencv
  - python3-pip
  - python3-setuptools
  - python3-sphinx