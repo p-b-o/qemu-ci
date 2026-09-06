d9055a40195c7443393c75944cae528f4e993117
Gilles Grimaud <gilles.grimaud@univ-lille.fr>
2026-09-06 21:44:01 +0200

    target/arm: allow Armv6-M to set CONTROL.nPRIV
    Armv6-M implements CONTROL.nPRIV even though it does not implement the Main
    Extension. The M-profile MSR helper currently gates writes to nPRIV on
    ARM_FEATURE_M_MAIN, preventing Cortex-M0 CPUs from entering unprivileged
    Thread mode.
    
    Allow privileged code to write nPRIV on every M-profile CPU. The translator
    already ends the translation block and rebuilds the execution flags after
    an M-profile MSR, so this does not require explicit TLB maintenance.
    
    Add a check-tcg test using the generic microbit machine. It verifies entry
    into unprivileged Thread mode, that unprivileged code cannot clear nPRIV,
    and that an exception handler can restore privileged Thread mode.
    
    Signed-off-by: Gilles Grimaud <gilles.grimaud@univ-lille.fr>
    Link: https://lore.kernel.org/qemu-devel/20260906194438.93046-3-gilles.grimaud@univ-lille.fr

--- target/arm/tcg/m_helper.c
+++ target/arm/tcg/m_helper.c
@@ -2775,7 +2775,7 @@ void HELPER(v7m_msr)(CPUARMState *env, uint32_t maskreg, uint32_t val)
                            !arm_v7m_is_handler_mode(env))) {
             write_v7m_control_spsel(env, (val & R_V7M_CONTROL_SPSEL_MASK) != 0);
         }
-        if (cur_el > 0 && arm_feature(env, ARM_FEATURE_M_MAIN)) {
+        if (cur_el > 0) {
             env->v7m.control[env->v7m.secure] &= ~R_V7M_CONTROL_NPRIV_MASK;
             env->v7m.control[env->v7m.secure] |= val & R_V7M_CONTROL_NPRIV_MASK;
         }
