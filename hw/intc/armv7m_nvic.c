f527b44b763fe670ff4f0dd296991a2269ab0522
Gilles Grimaud <gilles.grimaud@univ-lille.fr>
2026-09-06 21:44:00 +0200

    target/arm: enforce Armv6-M NVIC limits
    Armv6-M supports no more than 32 external interrupts. Its ICTR and Active
    Bit Register locations are reserved, and Cortex-M0 implements two priority
    bits.
    
    Reject Armv6-M NVIC configurations with more than 32 external interrupts.
    Add a check-tcg test that verifies the reserved register behavior and
    priority masking on the microbit machine.
    
    Signed-off-by: Gilles Grimaud <gilles.grimaud@univ-lille.fr>
    Link: https://lore.kernel.org/qemu-devel/20260906194438.93046-2-gilles.grimaud@univ-lille.fr

--- hw/intc/armv7m_nvic.c
+++ hw/intc/armv7m_nvic.c
@@ -2728,6 +2728,13 @@ static void armv7m_nvic_realize(DeviceState *dev, Error **errp)
         return;
     }
 
+    if (!arm_feature(&s->cpu->env, ARM_FEATURE_V7) &&
+        !arm_feature(&s->cpu->env, ARM_FEATURE_V8) &&
+        s->num_irq > 32) {
+        error_setg(errp, "Armv6-M NVIC cannot exceed 32 external IRQs");
+        return;
+    }
+
     qdev_init_gpio_in(dev, set_irq_level, s->num_irq);
 
     /* include space for internal exception vectors */
