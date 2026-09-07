23260002fbafd0eabe44b5db827e5f1b11b8bdbc
Gilles Grimaud <gilles.grimaud@univ-lille.fr>
2026-09-07 07:24:13 +0200

    hw/ssi: add RP2040 XIP flash controller
    Model the XIP windows, cache control registers, SSI interface, and external
    NOR flash commands. Add raw-file persistence, configurable flash UID, and
    focused qtests, and connect IO_QSPI chip-select overrides.
    
    Back the executable XIP windows with a ROM device so TCG can map guest code
    directly. Disable ROMD while the flash is busy to route accesses through
    the device callbacks and preserve XIP faults. Flush translated mappings
    after flash contents change, and implement the alternate XIP views as
    aliases of the same ROM device.
    
    Update the synthetic boot path and existing TCG guests to execute a
    compiled second-stage boot block. The boot2 source is derived from the
    Raspberry Pi Pico SDK and therefore retains its BSD-3-Clause license.
    
    Signed-off-by: gilles grimaud <gilles.grimaud@univ-lille.fr>
    Link: https://lore.kernel.org/qemu-devel/20260907052426.24849-9-gilles.grimaud@univ-lille.fr

--- hw/arm/raspi_pico.c
+++ hw/arm/raspi_pico.c
@@ -14,6 +14,7 @@
 #include "hw/arm/rp2040.h"
 #include "hw/core/boards.h"
 #include "hw/core/qdev-properties.h"
+#include "qemu/cutils.h"
 #include "system/address-spaces.h"
 #include "system/system.h"
 #include "qom/object.h"
@@ -21,17 +22,80 @@
 #define TYPE_RASPI_PICO_MACHINE MACHINE_TYPE_NAME("raspi-pico")
 OBJECT_DECLARE_SIMPLE_TYPE(RaspiPicoMachineState, RASPI_PICO_MACHINE)
 
-#define PICO_FLASH_SIZE (2 * MiB)
-
 struct RaspiPicoMachineState {
     MachineState parent_obj;
 
     RP2040State soc;
-    MemoryRegion flash;
+    char *flash_file;
+    char *flash_uid;
+    uint64_t flash_uid_value;
+    bool flash_uid_set;
     uint64_t rosc_random_seed;
     bool rosc_random_seed_set;
     bool strict_uart_pins;
 };
+
+static char *raspi_pico_get_flash_file(Object *obj, Error **errp)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    return g_strdup(s->flash_file ?: "");
+}
+
+static void raspi_pico_set_flash_file(Object *obj, const char *value,
+                                      Error **errp)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    g_free(s->flash_file);
+    s->flash_file = g_strdup(value);
+}
+
+static char *raspi_pico_get_flash_uid(Object *obj, Error **errp)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    return g_strdup(s->flash_uid ?: "");
+}
+
+static void raspi_pico_set_flash_uid(Object *obj, const char *value,
+                                     Error **errp)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+    const char *p;
+    uint64_t uid;
+    int i;
+
+    if (!value || !*value) {
+        g_free(s->flash_uid);
+        s->flash_uid = NULL;
+        s->flash_uid_value = 0;
+        s->flash_uid_set = false;
+        return;
+    }
+
+    p = g_str_has_prefix(value, "0x") || g_str_has_prefix(value, "0X") ?
+        value + 2 : value;
+    if (strlen(p) != 16) {
+        error_setg(errp, "flash-uid must be exactly 16 hexadecimal digits");
+        return;
+    }
+    for (i = 0; i < 16; i++) {
+        if (!g_ascii_isxdigit(p[i])) {
+            error_setg(errp, "invalid flash-uid '%s'", value);
+            return;
+        }
+    }
+    if (qemu_strtou64(p, NULL, 16, &uid) < 0) {
+        error_setg(errp, "invalid flash-uid '%s'", value);
+        return;
+    }
+
+    g_free(s->flash_uid);
+    s->flash_uid = g_strdup(value);
+    s->flash_uid_value = uid;
+    s->flash_uid_set = true;
+}
 
 static void raspi_pico_get_rosc_random_seed(Object *obj, Visitor *v,
                                             const char *name, void *opaque,
@@ -73,6 +137,14 @@ static void raspi_pico_init(MachineState *machine)
     qdev_prop_set_bit(DEVICE(&s->soc.rosc), "random-seed-set",
                       s->rosc_random_seed_set);
     qdev_prop_set_uint32(DEVICE(&s->soc.sio), "gpio-hi-in", 1u << 1);
+    if (s->flash_file) {
+        qdev_prop_set_string(DEVICE(&s->soc.xip), "flash-file",
+                             s->flash_file);
+    }
+    if (s->flash_uid_set) {
+        qdev_prop_set_uint64(DEVICE(&s->soc.xip), "flash-uid",
+                             s->flash_uid_value);
+    }
     if (machine->firmware) {
         qdev_prop_set_string(DEVICE(&s->soc), "bootrom-file",
                              machine->firmware);
@@ -80,14 +152,21 @@ static void raspi_pico_init(MachineState *machine)
     object_property_set_link(OBJECT(&s->soc), "memory",
                              OBJECT(system_memory), &error_fatal);
 
-    memory_region_init_rom(&s->flash, NULL, "raspi-pico.flash",
-                           PICO_FLASH_SIZE, &error_fatal);
-    memory_region_add_subregion(system_memory, RP2040_XIP_BASE, &s->flash);
-
     sysbus_realize(SYS_BUS_DEVICE(&s->soc), &error_fatal);
 
-    armv7m_load_kernel(s->soc.armv7m[0].cpu, machine->kernel_filename,
-                       RP2040_XIP_BASE, PICO_FLASH_SIZE);
+    rp2040_xip_load_image(&s->soc.xip, machine->kernel_filename,
+                          &error_fatal);
+    armv7m_load_kernel(s->soc.armv7m[0].cpu, NULL,
+                       RP2040_XIP_BASE, 2 * MiB);
+    rp2040_xip_set_writable(&s->soc.xip, false);
+}
+
+static void raspi_pico_machine_finalize(Object *obj)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    g_free(s->flash_file);
+    g_free(s->flash_uid);
 }
 
 static bool raspi_pico_get_strict_uart_pins(Object *obj, Error **errp)
@@ -126,6 +205,19 @@ static void raspi_pico_machine_class_init(ObjectClass *oc, const void *data)
     mc->no_floppy = 1;
     mc->no_cdrom = 1;
 
+    object_class_property_add_str(oc, "flash-file",
+                                  raspi_pico_get_flash_file,
+                                  raspi_pico_set_flash_file);
+    object_class_property_set_description(oc, "flash-file",
+                                          "Load initial XIP flash contents "
+                                          "from a raw host file");
+    object_class_property_add_str(oc, "flash-uid",
+                                  raspi_pico_get_flash_uid,
+                                  raspi_pico_set_flash_uid);
+    object_class_property_set_description(oc, "flash-uid",
+                                          "Set the emulated external flash "
+                                          "64-bit unique ID as 16 hex digits");
+
     object_class_property_add(oc, "rosc-random-seed", "uint64",
                               raspi_pico_get_rosc_random_seed,
                               raspi_pico_set_rosc_random_seed, NULL, NULL);
@@ -147,6 +239,7 @@ static const TypeInfo raspi_pico_machine_info = {
     .parent = TYPE_MACHINE,
     .instance_size = sizeof(RaspiPicoMachineState),
     .instance_init = raspi_pico_machine_initfn,
+    .instance_finalize = raspi_pico_machine_finalize,
     .class_init = raspi_pico_machine_class_init,
 };
 
