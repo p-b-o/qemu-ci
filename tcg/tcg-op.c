afb4f06f3bb305a12ce50852f4768fda1a95cc3c
Richard Henderson <richard.henderson@linaro.org>
2026-09-16 18:28:17 -1000

    tcg: Generic expansion of tcg_gen_ld16u_*
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Link: https://lore.kernel.org/qemu-devel/20260917042823.542588-96-richard.henderson@linaro.org

--- tcg/tcg-op.c
+++ tcg/tcg-op.c
@@ -915,6 +915,12 @@ static void gen_ld16s(TCGType type, TCGTemp *dst,
     gen_op_tti(INDEX_op_ld16s, type, dst, base, ofs);
 }
 
+static void gen_ld16u(TCGType type, TCGTemp *dst,
+                      TCGTemp *base, tcg_target_long ofs)
+{
+    gen_op_tti(INDEX_op_ld16u, type, dst, base, ofs);
+}
+
 static void gen_mov(TCGType type, TCGTemp *dst, TCGTemp *src)
 {
     if (src != dst) {
@@ -1519,11 +1525,6 @@ void tcg_gen_revbit32_i32(TCGv_i32 ret, TCGv_i32 arg)
     }
 }
 
-void tcg_gen_ld16u_i32(TCGv_i32 ret, TCGv_ptr arg2, tcg_target_long offset)
-{
-    tcg_gen_ldst_op_i32(INDEX_op_ld16u, ret, arg2, offset);
-}
-
 void tcg_gen_st8_i32(TCGv_i32 arg1, TCGv_ptr arg2, tcg_target_long offset)
 {
     tcg_gen_ldst_op_i32(INDEX_op_st8, arg1, arg2, offset);
@@ -1541,11 +1542,6 @@ void tcg_gen_st_i32(TCGv_i32 arg1, TCGv_ptr arg2, tcg_target_long offset)
 
 
 /* 64-bit ops */
-
-void tcg_gen_ld16u_i64(TCGv_i64 ret, TCGv_ptr arg2, tcg_target_long offset)
-{
-    tcg_gen_ldst_op_i64(INDEX_op_ld16u, ret, arg2, offset);
-}
 
 void tcg_gen_ld32u_i64(TCGv_i64 ret, TCGv_ptr arg2, tcg_target_long offset)
 {
