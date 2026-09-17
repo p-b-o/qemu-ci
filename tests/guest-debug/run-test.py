c9c5558b0cf83c7831fb705c96980d244328d706
Richard Henderson <richard.henderson@linaro.org>
2026-09-17 12:07:17 -1000

    tests/guest-debug/run-test: Add --pargs
    Add --pargs <string> to pass command-line arguments to
    the user-mode program.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Link: https://lore.kernel.org/qemu-devel/20260917220718.60899-2-richard.henderson@linaro.org

--- tests/guest-debug/run-test.py
+++ tests/guest-debug/run-test.py
@@ -24,6 +24,7 @@ def get_args():
     parser.add_argument("--qemu", help="Qemu binary for test",
                         required=True)
     parser.add_argument("--qargs", help="Qemu arguments for test")
+    parser.add_argument("--pargs", help="Program arguments for test")
     parser.add_argument("--binary", help="Binary to debug",
                         required=True)
     parser.add_argument("--test", help="GDB test script")
@@ -88,6 +89,8 @@ if __name__ == '__main__':
             suspend = ''
         cmd = f'{args.qemu} {args.qargs} -g {socket_name}{suspend}' \
             f' {args.binary}'
+        if args.pargs:
+            cmd += f' {args.pargs}'
 
     log(output, "QEMU CMD: %s" % (cmd))
     inferior = subprocess.Popen(shlex.split(cmd))
