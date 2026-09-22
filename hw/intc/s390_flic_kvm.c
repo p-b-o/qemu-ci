6777eeea1c9c299c09d73bcd2aa7dde7d822ee01
Eric Farman <farman@linux.ibm.com>
2026-09-22 22:15:23 +0200

    hw/intc/s390x: Fix check after g_try_realloc
    The loop to store pending IRQs uses g_try_realloc() to expand the
    buffer being used, but the returned address is stored in a
    pointer-to-pointer, thus the check is supposed to be examining
    the contents of the pointer, and not the pointer itself.
    
    Define a temporary variable to catch and test the returned pointer.
    If it's NULL, return to the caller to perform the free on the
    unmodified buffer. If it's not NULL, store it in the caller's
    pointer and restart the loop against the new struct.
    
    Cc: qemu-stable@nongnu.org
    Fixes: 3a553fc658 ("s390x/kvm: implement floating-interrupt controller device")
    Reviewed-by: Christian Borntraeger <borntraeger@linux.ibm.com>
    Reviewed-by: Matthew Rosato <mjrosato@linux.ibm.com>
    Signed-off-by: Eric Farman <farman@linux.ibm.com>
    Link: https://lore.kernel.org/qemu-devel/20260922201526.2383047-8-farman@linux.ibm.com

--- hw/intc/s390_flic_kvm.c
+++ hw/intc/s390_flic_kvm.c
@@ -254,6 +254,7 @@ static int __get_all_irqs(KVMS390FLICState *flic,
                           void **buf, int len)
 {
     int r;
+    void *new_buf = NULL;
 
     do {
         /* returns -ENOMEM if buffer is too small and number
@@ -263,10 +264,11 @@ static int __get_all_irqs(KVMS390FLICState *flic,
             break;
         }
         len *= 2;
-        *buf = g_try_realloc(*buf, len);
-        if (!buf) {
+        new_buf = g_try_realloc(*buf, len);
+        if (!new_buf) {
             return -ENOMEM;
         }
+        *buf = new_buf;
     } while (r == -ENOMEM && len <= KVM_S390_FLIC_MAX_BUFFER);
 
     return r;
